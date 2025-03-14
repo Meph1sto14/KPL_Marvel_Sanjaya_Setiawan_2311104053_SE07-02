01eb89b4c4c3b98931673b10258ec0be579e414f
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-03-14 14:16:21 +0700

    membuat branch generic class

---
+++ 05_Generics/tpmodul5_2311104053/Program.cs
@@ -0,0 +1,23 @@
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> data = new DataGeneric<string>("2311104053");
+        data.PrintData();
+    }
+}
