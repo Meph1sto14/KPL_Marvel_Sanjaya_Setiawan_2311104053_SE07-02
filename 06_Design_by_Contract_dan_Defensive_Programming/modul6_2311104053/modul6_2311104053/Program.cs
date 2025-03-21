6a5a5355b7a4e3fc6cd7eff61c174b61a3bbb53f
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-03-21 09:30:29 +0700

    menambahkan testing Program.cs

--- 06_Design_by_Contract_dan_Defensive_Programming/modul6_2311104053/modul6_2311104053/Program.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/modul6_2311104053/modul6_2311104053/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace modul6_2311104053
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            try
+            {
+                SayaTubeUser user = new SayaTubeUser("Nama Praktikan");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    SayaTubeVideo video = new SayaTubeVideo($"Review Film {i} oleh Nama Praktikan");
+                    user.AddVideo(video);
+                }
+
+                user.PrintAllVideoPlaycount();
+
+                SayaTubeVideo videoTest = new SayaTubeVideo("Test Video");
+                videoTest.IncreasePlayCount(25000001);
+
+                user.PrintAllVideoPlaycount();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
