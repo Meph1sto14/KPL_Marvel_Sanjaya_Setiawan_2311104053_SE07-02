968c62b66b43155fde77179a0cda74f2c31146e2
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-05-07 16:50:08 +0700

    upload jurnal modul 10

---
+++ 10_Library_Construction/JURNAL/modul10_2311104053/ConsoleAppMatematika/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("FPB(60, 45)");
+        Console.WriteLine($"Output: {Matematika.FPB(60, 45)}\n");
+
+        Console.WriteLine("KPK(12, 8)");
+        Console.WriteLine($"Output: {Matematika.KPK(12, 8)}\n");
+
+        int[] fungsiTurunan = { 1, 4, -12, 9 };
+        Console.WriteLine("Turunan({1, 4, -12, 9})");
+        Console.WriteLine($"Output: \"{Matematika.Turunan(fungsiTurunan)}\"\n");
+
+        int[] fungsiIntegral = { 4, 6, -12, 9 };
+        Console.WriteLine("Integral({4, 6, -12, 9})");
+        Console.WriteLine($"Output: \"{Matematika.Integral(fungsiIntegral)}\"");
+    }
+}
---
+++ 10_Library_Construction/JURNAL/modul10_2311104053/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace MatematikaLibraries
+{
+    public static class Matematika
+    {
+        public static int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public static string Turunan(int[] koefisien)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int derajatAwal = koefisien.Length - 1;
+
+            for (int i = 0; i < koefisien.Length - 1; i++)
+            {
+                int pangkatBaru = derajatAwal - i - 1;
+                int koefTurunan = koefisien[i] * (derajatAwal - i);
+
+                if (koefTurunan == 0)
+                    continue;
+
+                if (hasil.Length > 0)
+                    hasil.Append(koefTurunan > 0 ? " + " : " - ");
+                else if (koefTurunan < 0)
+                    hasil.Append("-");
+
+                int absKoef = Math.Abs(koefTurunan);
+                if (absKoef != 1 || pangkatBaru == 0)
+                    hasil.Append(absKoef);
+
+                if (pangkatBaru > 1)
+                    hasil.Append($"x{pangkatBaru}");
+                else if (pangkatBaru == 1)
+                    hasil.Append("x");
+            }
+
+            return hasil.ToString();
+        }
+
+
+        public static string Integral(int[] koefisien)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int pangkat = koefisien.Length;
+
+            for (int i = 0; i < koefisien.Length; i++)
+            {
+                double nilai = (double)koefisien[i] / pangkat;
+
+                if (hasil.Length > 0)
+                    hasil.Append(nilai > 0 ? " + " : " - ");
+                else if (nilai < 0)
+                    hasil.Append("-");
+
+                double absNilai = Math.Abs(nilai);
+                if (absNilai != 1)
+                    hasil.Append($"{absNilai}");
+
+                if (pangkat > 1)
+                    hasil.Append($"x{pangkat}");
+                else if (pangkat == 1)
+                    hasil.Append("x");
+
+                pangkat--;
+            }
+
+            hasil.Append(" + C");
+            return hasil.ToString();
+        }
+
+    }
+}
