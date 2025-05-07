941320f711623f51982d22d8bf36bf59e12ca1bb
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-05-07 17:16:29 +0700

    upload tp modul 12 Performance Analysis

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TP/tpmodul12_2311104053/tpmodul12_2311104053/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmodul12_2311104053
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input;
+            if (int.TryParse(textBox1.Text, out input))
+            {
+                string hasil = CariTandaBilangan(input);
+                label1.Text = hasil;
+            }
+            else
+            {
+                label1.Text = "Input tidak valid!";
+            }
+        }
+
+        private string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+
+
+    }
+}
