ea1ece67731d190f1fb0d0e18f466b1a70d3c257
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-05-15 19:03:08 +0700

    upload ulang modul 13 design pattern observer

--- 13_Design_Pattern_Implementation/TP/tpmodul13_2311104053/tpmodul13_2311104053/Program.cs
+++ 13_Design_Pattern_Implementation/TP/tpmodul13_2311104053/tpmodul13_2311104053/Program.cs
@@ -1,150 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
 {
-    // The Abstract Factory interface declares a set of methods that return
-    // different abstract products. These products are called a family and are
-    // related by a high-level theme or concept. Products of one family are
-    // usually able to collaborate among themselves. A family of products may
-    // have several variants, but the products of one variant are incompatible
-    // with products of another.
-    public interface IAbstractFactory
+    public interface IObserver
     {
-        IAbstractProductA CreateProductA();
-
-        IAbstractProductB CreateProductB();
+        // Receive update from subject
+        void Update(ISubject subject);
     }
 
-    // Concrete Factories produce a family of products that belong to a single
-    // variant. The factory guarantees that resulting products are compatible.
-    // Note that signatures of the Concrete Factory's methods return an abstract
-    // product, while inside the method a concrete product is instantiated.
-    class ConcreteFactory1 : IAbstractFactory
+    public interface ISubject
     {
-        public IAbstractProductA CreateProductA()
-        {
-            return new ConcreteProductA1();
-        }
+        // Attach an observer to the subject.
+        void Attach(IObserver observer);
 
-        public IAbstractProductB CreateProductB()
-        {
-            return new ConcreteProductB1();
-        }
+        // Detach an observer from the subject.
+        void Detach(IObserver observer);
+
+        // Notify all observers about an event.
+        void Notify();
     }
 
-    // Each Concrete Factory has a corresponding product variant.
-    class ConcreteFactory2 : IAbstractFactory
+    // The Subject owns some important state and notifies observers when the
+    // state changes.
+    public class Subject : ISubject
     {
-        public IAbstractProductA CreateProductA()
+        // For the sake of simplicity, the Subject's state, essential to all
+        // subscribers, is stored in this variable.
+        public int State { get; set; } = -0;
+
+        // List of subscribers. In real life, the list of subscribers can be
+        // stored more comprehensively (categorized by event type, etc.).
+        private List<IObserver> _observers = new List<IObserver>();
+
+        // The subscription management methods.
+        public void Attach(IObserver observer)
         {
-            return new ConcreteProductA2();
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
         }
 
-        public IAbstractProductB CreateProductB()
+        public void Detach(IObserver observer)
         {
-            return new ConcreteProductB2();
-        }
-    }
-
-    // Each distinct product of a product family should have a base interface.
-    // All variants of the product must implement this interface.
-    public interface IAbstractProductA
-    {
-        string UsefulFunctionA();
-    }
-
-    // Concrete Products are created by corresponding Concrete Factories.
-    class ConcreteProductA1 : IAbstractProductA
-    {
-        public string UsefulFunctionA()
-        {
-            return "The result of the product A1.";
-        }
-    }
-
-    class ConcreteProductA2 : IAbstractProductA
-    {
-        public string UsefulFunctionA()
-        {
-            return "The result of the product A2.";
-        }
-    }
-
-    // Here's the the base interface of another product. All products can
-    // interact with each other, but proper interaction is possible only between
-    // products of the same concrete variant.
-    public interface IAbstractProductB
-    {
-        // Product B is able to do its own thing...
-        string UsefulFunctionB();
-
-        // ...but it also can collaborate with the ProductA.
-        //
-        // The Abstract Factory makes sure that all products it creates are of
-        // the same variant and thus, compatible.
-        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
-    }
-
-    // Concrete Products are created by corresponding Concrete Factories.
-    class ConcreteProductB1 : IAbstractProductB
-    {
-        public string UsefulFunctionB()
-        {
-            return "The result of the product B1.";
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
         }
 
-        // The variant, Product B1, is only able to work correctly with the
-        // variant, Product A1. Nevertheless, it accepts any instance of
-        // AbstractProductA as an argument.
-        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        // Trigger an update in each subscriber.
+        public void Notify()
         {
-            var result = collaborator.UsefulFunctionA();
+            Console.WriteLine("Subject: Notifying observers...");
 
-            return $"The result of the B1 collaborating with the ({result})";
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+
+        // Usually, the subscription logic is only a fraction of what a Subject
+        // can really do. Subjects commonly hold some important business logic,
+        // that triggers a notification method whenever something important is
+        // about to happen (or after it).
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("\nSubject: I'm doing something important.");
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("Subject: My state has just changed to: " + this.State);
+            this.Notify();
         }
     }
 
-    class ConcreteProductB2 : IAbstractProductB
+    // Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    class ConcreteObserverA : IObserver
     {
-        public string UsefulFunctionB()
+        public void Update(ISubject subject)
         {
-            return "The result of the product B2.";
-        }
-
-        // The variant, Product B2, is only able to work correctly with the
-        // variant, Product A2. Nevertheless, it accepts any instance of
-        // AbstractProductA as an argument.
-        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-        {
-            var result = collaborator.UsefulFunctionA();
-
-            return $"The result of the B2 collaborating with the ({result})";
+            if ((subject as Subject).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+            }
         }
     }
 
-    // The client code works with factories and products only through abstract
-    // types: AbstractFactory and AbstractProduct. This lets you pass any
-    // factory or product subclass to the client code without breaking it.
-    class Client
+    class ConcreteObserverB : IObserver
     {
-        public void Main()
+        public void Update(ISubject subject)
         {
-            // The client code can work with any concrete factory class.
-            Console.WriteLine("Client: Testing client code with the first factory type...");
-            ClientMethod(new ConcreteFactory1());
-            Console.WriteLine();
-
-            Console.WriteLine("Client: Testing the same client code with the second factory type...");
-            ClientMethod(new ConcreteFactory2());
-        }
-
-        public void ClientMethod(IAbstractFactory factory)
-        {
-            var productA = factory.CreateProductA();
-            var productB = factory.CreateProductB();
-
-            Console.WriteLine(productB.UsefulFunctionB());
-            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
         }
     }
 
@@ -152,7 +102,20 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
     {
         static void Main(string[] args)
         {
-            new Client().Main();
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
         }
     }
 }
