65e0a457dadf09af678d1218c94f7e0bad55bb5f
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-05-15 15:28:55 +0700

    membuat jurnal modul 12 tentang 12_Performance_Analysis_Unit_Testing_dan_Debugging

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/JURNAL/modul12_2311104053/modul12_2311104053/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace modul12_2311104053
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonHitung = new System.Windows.Forms.Button();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // buttonHitung
+            // 
+            this.buttonHitung.Location = new System.Drawing.Point(253, 116);
+            this.buttonHitung.Name = "buttonHitung";
+            this.buttonHitung.Size = new System.Drawing.Size(100, 23);
+            this.buttonHitung.TabIndex = 2;
+            this.buttonHitung.Text = "Calculate";
+            this.buttonHitung.UseVisualStyleBackColor = true;
+            this.buttonHitung.Click += new System.EventHandler(this.buttonHitung_Click);
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(395, 119);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(45, 16);
+            this.labelHasil.TabIndex = 3;
+            this.labelHasil.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.buttonHitung);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonHitung;
+        private System.Windows.Forms.Label labelHasil;
+    }
+}
+
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/JURNAL/modul12_2311104053/modul12_2311104053/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace modul12_2311104053
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonHitung_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+
+            int hasil = CariNilaiPangkat(a, b);
+            labelHasil.Text = hasil.ToString();
+        }
+
+        public int CariNilaiPangkat(int a, int b)
+        {
+            if (b == 0) return 1;
+            if (b < 0) return -1;
+            if (b > 10 || a > 100) return -2;
+
+            try
+            {
+                checked
+                {
+                    int result = 1;
+                    for (int i = 0; i < b; i++)
+                    {
+                        result *= a;
+                    }
+                    return result;
+                }
+            }
+            catch (OverflowException)
+            {
+                return -3;
+            }
+        }
+    }
+}
